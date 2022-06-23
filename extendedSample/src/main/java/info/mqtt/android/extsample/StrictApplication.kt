55874d7b2149c55af8a9a8a3e3c72581edfeaccd
Hannes Achleitner <hannes.software@gmx.at>
2022-06-23 10:45:53 +0200

    Strict mode

---
+++ extendedSample/src/main/java/info/mqtt/android/extsample/StrictApplication.kt
@@ -0,0 +1,20 @@
+package info.mqtt.android.extsample
+
+import android.os.StrictMode
+import info.hannes.logcat.FileLoggingApplication
+
+class StrictApplication : FileLoggingApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        StrictMode.setThreadPolicy(
+            StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectNetwork()
+                .penaltyDialog()
+                .build()
+        )
+    }
+}
